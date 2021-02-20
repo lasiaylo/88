29af612289fea00bd13d524dc4e86e167a02ffb1
Lasia Lo <lasiaylo@fb.com>
2021-02-20 15:29:06 -0500

    Guard + Damaged Sound effects

--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class PlayerHealth : Health
 {
     public PlayerGuard guard;
+    public AudioSource damageSound;
+    public AudioSource damageGuardedSound;
 
     public override void Damage(float damage)
     {
         if (guard.Status == GuardStatus.Guarding)
         {
             // Send Player Guard Event
+            damageGuardedSound.Play();
             Debug.Log("WHOAH! A block!");
         } else
         {
             // TODO: Send Player Damage Event
             // TODO: Screen Shake
             // TODO: Noise
+            damageSound.Play();
             HP.MinusOverTime(damage);
         }
     }
